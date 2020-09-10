30d229145617634b98cae2181ad12ae1bdda94ba
ronngo <70925598+ronngo@users.noreply.github.com>
2020-09-10 22:24:10 +0700

    Add files via upload

---
+++ toantu/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace toantu
+{
+    class Program
+    {
+        public float tinh(float width, float height)
+        {
+            float area = width * height;
+            Console.WriteLine("Area is: " + area);
+            return area;
+        }
+        static void Main(string[] args)
+        {
+            float width, height, kq;
+            Console.Write("Enter width: ");
+            width = float.Parse(Console.ReadLine());
+            Console.Write("Enter height: ");
+            height = float.Parse(Console.ReadLine());
+            Program n = new Program();
+            kq = n.tinh(width, height);
+        }
+    }
+}
